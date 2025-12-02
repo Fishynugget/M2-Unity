1af2e649ff9b7ba9d52db93dc9b2ec8fbe9a0c7d
Helen <40919@ma-web.nl>
2025-12-02 12:59:22 +0100

    3.1 af en Readme hopelijk gefixed

---
+++ Assets/scripts_peggle/Peggle.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class Peggle : MonoBehaviour
+{
+    public int hitsToDestroy = 1;     // totaal aantal hits dat deze peg aankan
+    public int pointsPerHit = 15;     // aantal punten dat één hit waard is
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // score toekennen
+        ScoreManager.Instance.AddScore(pointsPerHit);
+
+        // aftellen
+        hitsToDestroy--;
+
+        // check of de peg nu op is
+        if (hitsToDestroy <= 0)
+        {
+            Destroy(gameObject, 0.25f);
+            Destroy(this.gameObject, 0.25f);
+        } 
+    }
+
+}
